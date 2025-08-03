90b47e2ddd4fd22731a48a265d80b29ca6988b1f
TANICE-GAWD <2006princesharma@gmail.com>
2025-08-03 21:56:23 +0530

    Up for deployment

--- FRONTEND/src/components/TimeLine/TimeLine.jsx
+++ FRONTEND/src/components/TimeLine/TimeLine.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useRef } from 'react';
 import { FaCalendarAlt, FaRocket, FaLaptopCode, FaTrophy } from 'react-icons/fa';
-import './Timeline.css';
+import './TimeLine.css';
 
 
 const timelineData = [
